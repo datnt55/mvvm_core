c8cdf001f855a399d156edbbc5f783a7c7dc75fc
Duc Tran <yugi.it.vn@gmail.com>
2020-07-21 09:42:02 +0700

    add prefe

--- app/src/main/java/com/library/core/model/local/preference/PreferenceProperty.kt
+++ app/src/main/java/com/library/core/model/local/preference/PreferenceProperty.kt
@@ -3,6 +3,7 @@ package com.library.core.model.local.preference
 import android.content.SharedPreferences
 import androidx.annotation.WorkerThread
 import androidx.core.content.edit
+import com.google.gson.Gson
 import kotlin.properties.ReadWriteProperty
 import kotlin.reflect.KProperty
 
@@ -31,5 +32,36 @@ fun SharedPreferences.String(
 
     override fun setValue(thisRef: Any, property: KProperty<*>, value: String) {
         edit { putString(key, value) }
+    }
+}
+
+fun SharedPreferences.Boolean(
+    key: String ,
+    defaultValue: Boolean = true
+): ReadWriteProperty<Any, Boolean> = object : ReadWriteProperty<Any, Boolean> {
+    @WorkerThread
+    override fun getValue(thisRef: Any, property: KProperty<*>): Boolean {
+        return getBoolean(key, defaultValue)
+    }
+
+    override fun setValue(thisRef: Any, property: KProperty<*>, value: Boolean) {
+        edit { putBoolean(key, value) }
+    }
+}
+
+inline fun <reified T> SharedPreferences.Object(
+    key: String ,
+    defaultValue: T
+): ReadWriteProperty<Any, T> = object : ReadWriteProperty<Any, T> {
+    @WorkerThread
+    override fun getValue(thisRef: Any, property: KProperty<*>): T {
+        if (getString(key, null).isNullOrBlank())
+            return defaultValue
+        return Gson().fromJson(getString(key, null),T::class.java)
+    }
+
+    override fun setValue(thisRef: Any, property: KProperty<*>, value: T) {
+        val pref = Gson().toJson(value)
+        edit { putString(key, pref) }
     }
 }
